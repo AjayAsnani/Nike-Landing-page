d01fee6e8e201e13a876bc4c64ae73eadd9165ca
AjayAsnani <ajaysnani555@gmail.com>
2023-10-27 23:13:39 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Popularproducts from "./Popularproducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  CustomerReviews,
+  Footer,
+  Subscribe,
+  SuperQuality,
+  Popularproducts,
+  Services,
+  SpecialOffer,
+};
